06366fc1d472620fe389f731b96f6706e50b44a7
Matheus <germano.prog@gmail.com>
2021-09-21 17:58:24 -0300

    REFACTOR: Mudando tamanho do texto carrossel (html)

--- js/main.js
+++ js/main.js
@@ -15,3 +15,25 @@ async function apiOmdb(filme) {
     erroApi(error);
   }
 }
+
+
+// $("button").on("click", async function (event) {
+//   event.preventDefault();
+//   $("#inicio").css("display", "none");
+//   let valueData = $("#date").val();
+//   let url = `https://api.nasa.gov/planetary/apod?api_key=QRaDd5HWwVtjNo83M6DhwSdhxw9M2Ibxj798RL5w&date=${valueData}`;
+//   let api = await fetch(url).then((response) => response.json());
+//   $("body").css("background-image", `url(${api.url})`);
+//   $("body").css("background-repeat", "no-repeat");
+//   $("body").css("background-size", "cover");
+//   $("#corpo").css("display", "block");
+//   $("#titulo").html(`${api.title}`);
+//   $("#texto").html(`(${api.explanation})`);
+//   console.log(event);
+//   console.log(apiResponse);
+//   console.log(inputDate);
+
+//   if (valueData < 01 / 01 / 1996) {
+//     alert("Datas válidas somente a partir de 1996");
+//   }
+// });
