b2369c6bf88559cfb1f043e2a942333f1cf5ee78
Matheus <germano.prog@gmail.com>
2021-09-22 19:44:50 -0300

    FEATURE: Dom na main | Requisição de API

--- js/main.js
+++ js/main.js
@@ -1,100 +1,44 @@
-
-
-
-
-
-// async function apiOmdb(filme) {
-//   const apikey = "3b8b5699";
-//   let url = `http://www.omdbapi.com/?i=${movieId}&apikey=${apikey}&`;
-//   let response = await fetch(url).then((response) => response.json());
-//   console.log(response);
-//   try {
-//     if (response.Poster !== undefined) {
-//       sucessoApi(response);
-//     } else {
-//       throw new Error(
-//         "Poster não encontrado"
-//       );
-//     }
-//   } catch (error) {
-//     erroApi(error);
-//   }
-// }
-
-
-
-async function pegaFilme(){
-  const apikey = "3b8b5699";
-  const movieId = "tt0077651";
-  let url = `http://www.omdbapi.com/?i=${movieId}&apikey=${apikey}&`;
-  let response = await fetch(url).then((response) => response.json());
-  console.log(response);
+async function apiFilme(filme) {
+  const key = "3b8b5699";
+  let url = `https://www.omdbapi.com/?apikey=${key}&t=${filme}`;
+  let apiResponse = await fetch(url).then((response) => response.json());
+  console.log(apiResponse);
   try {
-    if (response.Poster !== undefined) {
-      $(`#filme1`).html(`<img class="img-fluid" src="${response.Poster}"/>
-      <div class="filme_info">
-          <div class="col-12">
-              <a href="#" class="btn-custom-round btn btn-light rounded-circle">
-                  <span class="mdi mdi-information-variant"></span>
-              </a>
-              <p>Informações do filme</p>
-          </div>
-      </div>`)
+    if (apiResponse.Title !== undefined) {
+      sucessoApi(apiResponse);
     } else {
       throw new Error(
-        "Poster não encontrado"
+        "O filme não foi encontrado, nosso buscador funciona apenas com titulos em seu idioma <strong>original</strong>. <br> Tente pesquisar em <strong>inglês.</strong>"
       );
     }
   } catch (error) {
-    console.log(error);
+    erroApi(error);
   }
 }
-window.onload = () =>{
-  load(pegaFilme())
+
+function sucessoApi(apiResponse) {
+  $(".list-content").css("display", "none");
+  $("#InfoOmdb").css("display", "flex");
+  $("#poster").html(`<img src="${apiResponse.Poster}">`);
+  $("#titulo").html(`${apiResponse.Title}`);
+  $("#diretor").html(`Diretor: ${apiResponse.Director}`);
+  $("#ano").html(`Ano: ${apiResponse.Year}`);
+  $("#duracao").html(`Duração: ${apiResponse.Runtime}`);
+  $("#sinopse").html(`Sinopse: ${apiResponse.Plot}`);
+  $("#btnPesquisa").html(
+    `<button id="botao"><a href="./html/buscar.html" class="text-light" id="tirar">Pesquise mais filmes</a></button>`
+  );
 }
 
-
-
-  // $.ajax({
-  //     url: `http://www.omdbapi.com/?i=${movieId}&apikey=${apikey}&`,
-  //     type: 'GET',
-  //     dataType: 'json',
-  //     beforeSend: function(){
-  //         $('#filme1').html(`src="${json.Poster}"`)
-  //     },
-  //     success:function(json){
-  //       var html = '';
-  //       for (var i in json){
-  //           html += 'src="'+ json[i].poster +'">'+json[i].titulo+'';
-  //       }
-  //       $('#filme').html(html)
-  //     }
-  // })
-
-
-
-// function insereFilme(){
-// let movieId = "tt0077651" 
-// ${"#filme1"}.appendchild.${"src"}.html(response.Poster)
-// }
-
-// $("button").on("click", async function (event) {
-//   event.preventDefault();
-//   $("#inicio").css("display", "none");
-//   let valueData = $("#date").val();
-//   let url = `https://api.nasa.gov/planetary/apod?api_key=QRaDd5HWwVtjNo83M6DhwSdhxw9M2Ibxj798RL5w&date=${valueData}`;
-//   let api = await fetch(url).then((response) => response.json());
-//   $("body").css("background-image", `url(${api.url})`);
-//   $("body").css("background-repeat", "no-repeat");
-//   $("body").css("background-size", "cover");
-//   $("#corpo").css("display", "block");
-//   $("#titulo").html(`${api.title}`);
-//   $("#texto").html(`(${api.explanation})`);
-//   console.log(event);
-//   console.log(apiResponse);
-//   console.log(inputDate);
-
-//   if (valueData < 01 / 01 / 1996) {
-//     alert("Datas válidas somente a partir de 1996");
-//   }
-// });
+function erroApi(error) {
+  $("#teste").css("display", "none");
+  $(".list-content").css("display", "none");
+  $("#infoOmdb").css("display", "flex");
+  $("#poster").empty();
+  $("#diretor").empty();
+  $("#ano").empty();
+  $("#lancamento").empty();
+  $("#duracao").empty();
+  $("#sinopse").empty();
+  $("#titulo").html(`${error}`);
+}
